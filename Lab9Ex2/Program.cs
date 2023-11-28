18751545d21b91ab2c212470114979f052586b69
Laiba Faisal <lfj1310@gmail.com>
2023-11-28 17:16:48 +0500

    My First Commit

---
+++ Lab9Ex2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+class Program {
+    static void Main(string[] args)
+    {
+        string cs = "Data Source=localhost\\SQLEXPRESS;Initial Catalog=Assignment3;Integrated Security=True";
+        string query = " SELECT customer_id, companyName from Customer";
+        SqlConnection con = new SqlConnection(cs);
+
+        con.Open();
+        SqlDataAdapter da = new SqlDataAdapter(query, con);
+        DataSet ds = new DataSet();
+        da.Fill(ds);
+        con.Close();
+
+        DataTable dataTable = ds.Tables[0];
+        Console.WriteLine("Customer ID  Company Name");
+        if (dataTable != null)
+        {
+            foreach (DataRow row in dataTable.Rows)
+            {
+                foreach (DataColumn column in dataTable.Columns)
+                {
+                    Console.Write($"{row[column]}\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+    } 
+}
---
+++ Lab9_Ex1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.Common;
+using System.Data.SqlTypes;
+
+namespace Lab9_Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cs = "Data Source=localhost\\SQLEXPRESS;Initial Catalog=Assignment3;Integrated Security=True;Encrypt=True";
+            string query = "SELECT Product_ID, Name, UnitPrice from Product WHERE unitprice > paramValue ORDER BY unitprice DESC";
+            int pricePoint = 30;
+            SqlConnection con = new SqlConnection(cs);
+            SqlCommand cmd = new SqlCommand(query, con);
+            cmd.Parameters.AddWithValue("@paramValue", pricePoint);
+
+            try
+            {
+                con.Open();
+                SqlDataReader dataReader = cmd.ExecuteReader();
+                while (dataReader.Read())
+                {
+                    Console.WriteLine("\t{0}\t{1}\t{2}", dataReader[0], dataReader[1], dataReader[2]);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
